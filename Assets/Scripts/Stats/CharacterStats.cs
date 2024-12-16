a7c8be082a918aec90853e4ccf22c51bcaf1db6d
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2024-12-16 17:55:54 +0700

    Add major stats and defensive stats

--- Assets/Scripts/Stats/CharacterStats.cs
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat strenght;
-    public Stat damage;
+    [Header("Major stats")]
+    public Stat strength; //1 point => increase damamge by 1 and crit dmg by 1% 
+    public Stat agility; //1 point => increase evasion by 1 and crit rate by 0.5%
+    public Stat intelligence; //1 point => increase magic by 1 and magic res by 3
+    public Stat vitality; //1 point => increase health by 5
+
+    [Header("Defensive stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+
+    public Stat damage;
     [SerializeField] private int currentHealth; 
     public virtual void Start()
     {
@@ -13,8 +22,12 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamge(CharacterStats _targetStats)
     {
-        int totalDamage = damage.GetValue() + strenght.GetValue();
+        if(CanDogdeAttack(_targetStats))
+            return;
+        
+        int totalDamage = damage.GetValue() + strength.GetValue();
 
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
         _targetStats.TakeDamage(totalDamage);
     }
 
@@ -33,4 +46,23 @@ public class CharacterStats : MonoBehaviour
         // Add death logic here
         Debug.Log("Dead");
     }
+
+    private bool CanDogdeAttack(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+        if(Random.Range(0, 100) < totalEvasion)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage -= _targetStats.armor.GetValue();
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        return totalDamage;
+    }
+    
 }

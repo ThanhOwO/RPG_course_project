94c0b9bb09bef4c234df7c3c1a2bb34d3570e043
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2024-12-18 17:54:05 +0700

    Add shrink effect to health bar

--- Assets/Scripts/UI_Scripts/HealthBar_UI.cs
+++ Assets/Scripts/UI_Scripts/HealthBar_UI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,31 +7,57 @@ public class HealthBar_UI : MonoBehaviour
     private Entity entity;
     private RectTransform myTransform;
     private CharacterStats myStats;
-    private Slider slider;
+    [SerializeField] private Slider topSlider; //health slider
+    [SerializeField] private Slider bottomSlider; //damage slider
+    private const float DAMAGED_HEALTH_SHRINK_TIMER_MAX = 0.5f;
+    private float damagedHealthShrinkTimer;
+    [SerializeField] private float shrinkSpeed;
 
     private void Start()
     {
         myTransform = GetComponent<RectTransform>();
         entity = GetComponentInParent<Entity>();
-        slider = GetComponentInChildren<Slider>();
+        topSlider = transform.Find("TopSlider").GetComponent<Slider>();
+        bottomSlider = transform.Find("BottomSlider").GetComponent<Slider>();
         myStats = GetComponentInParent<CharacterStats>();
         
         entity.onFlipped += FlipUI; //When the onFlipped happens we add an call the FlipUI function
         myStats.onHealthChanged += UpdateHealthUI;
 
         UpdateHealthUI();
-
+        bottomSlider.value = topSlider.value;
     }
 
     private void Update() 
     {
-        UpdateHealthUI();
+        ShrinkDamageBar();
     }
 
     private void UpdateHealthUI()
     {
-        slider.maxValue = myStats.GetMaxHealthValue();
-        slider.value = myStats.currentHealth;;
+        // Alex's Udemy code
+        // slider.maxValue = myStats.GetMaxHealthValue();
+        // slider.value = myStats.currentHealth;;
+
+        //My code
+        damagedHealthShrinkTimer = DAMAGED_HEALTH_SHRINK_TIMER_MAX;
+        topSlider.maxValue = myStats.GetMaxHealthValue();
+        bottomSlider.maxValue = myStats.GetMaxHealthValue();
+
+        topSlider.value = myStats.currentHealth;
+    }
+
+    private void ShrinkDamageBar()
+    {
+        damagedHealthShrinkTimer -= Time.deltaTime;
+
+        if(damagedHealthShrinkTimer < 0)
+        {
+            if(bottomSlider.value > topSlider.value)
+            {
+                bottomSlider.value -= shrinkSpeed * Time.deltaTime;
+            }
+        }
     }
 
     private void FlipUI() => myTransform.Rotate(0, 180, 0);

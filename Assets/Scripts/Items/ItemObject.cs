61694cbbd35546239624678bccb5d695202448f5
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2024-12-23 15:39:42 +0700

    Create ItemData & ItemObject

---
+++ Assets/Scripts/Items/ItemObject.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    [SerializeField] private ItemData itemData;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        sr.sprite = itemData.icon;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if(collision.GetComponent<Player>() != null)
+        {
+            Debug.Log("Picked up the item " + itemData.itemName);
+            Destroy(gameObject);
+        }
+    }
+}

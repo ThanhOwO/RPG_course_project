bbf05984edad9de7cee2b2383eafadf818d4c360
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2024-12-25 10:22:40 +0700

    Add enemy stat modifier

--- Assets/Scripts/Stats/EnemyStats.cs
+++ Assets/Scripts/Stats/EnemyStats.cs
@@ -4,10 +4,46 @@ public class EnemyStats : CharacterStats
 {
     private Enemy enemy;
 
+    [Header("Level details")]
+    [SerializeField] private int level = 1;
+
+    [Range(0f,1f)]
+    [SerializeField] private float percentageModifier = 0.4f; 
+
     protected override void Start()
     {
+        //Lvl apply must be called first
+        ApplyLevelModifier();
+
         base.Start();
         enemy = GetComponent<Enemy>();
+
+    }
+
+    private void ApplyLevelModifier()
+    {
+        //Enemy basic level modifier stats
+        Modify(damage);
+
+        Modify(maxHealth);
+        Modify(armor);
+        Modify(magicRes);
+        Modify(evasion);
+
+        Modify(fireDmg);
+        Modify(iceDmg);
+        Modify(lightningDmg);
+        Modify(poisonDmg);
+    }
+
+    //Increase enemy power by level and percentage
+    private void Modify(Stat _stat)
+    {
+        for(int i = 0; i < level; i++)
+        {
+            float modifier = _stat.GetValue() * percentageModifier;
+            _stat.AddModifier(Mathf.RoundToInt(modifier));
+        }
     }
 
     public override void TakeDamage(int _damage)

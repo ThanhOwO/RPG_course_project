448236a07bb74bb645d86aa89a3a431d37c31271
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2024-12-31 10:09:38 +0700

    Create switch UI key on keyboard

--- Assets/Scripts/UI_Scripts/UI.cs
+++ Assets/Scripts/UI_Scripts/UI.cs
@@ -2,28 +2,54 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
+    [SerializeField] private GameObject characterUI;
+    [SerializeField] private GameObject skillTreeUI;
+    [SerializeField] private GameObject craftUI;
+    [SerializeField] private GameObject optionUI;
+    [SerializeField] private GameObject playerHealthUI;
     public UI_ItemTooltip itemTooltip;
     public UI_StatToolTip statTooltip;
 
     void Start()
     {
-
+        SwitchTo(null);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Tab))
+            SwitchWithKeyTo(characterUI);
+        if(Input.GetKeyDown(KeyCode.C))
+            SwitchWithKeyTo(craftUI);
+        if(Input.GetKeyDown(KeyCode.Escape))
+            SwitchWithKeyTo(optionUI);
+        if(Input.GetKeyDown(KeyCode.I))
+            SwitchWithKeyTo(skillTreeUI);
     }
 
     public void SwitchTo(GameObject _menu)
     {
         for(int i = 0; i < transform.childCount; i++)
         {
-            transform.GetChild(i).gameObject.SetActive(false);
+            GameObject child = transform.GetChild(i).gameObject;
+
+            if(child != playerHealthUI)
+                child.SetActive(false);
         }
 
         if(_menu != null)
             _menu.SetActive(true);
+    }
+
+    public void SwitchWithKeyTo(GameObject _menu)
+    {
+        if(_menu != null && _menu.activeSelf)
+        {
+            _menu.SetActive(false);
+            return;
+        }
+
+        SwitchTo(_menu);
     }
 }

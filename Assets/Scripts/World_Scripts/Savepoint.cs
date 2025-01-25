be2bea222717ae99c2572dbb8190939bf23993cc
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2025-01-26 01:03:32 +0700

    Adding new tile map set, create light system, adjust save point light

--- Assets/Scripts/World_Scripts/Savepoint.cs
+++ Assets/Scripts/World_Scripts/Savepoint.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Savepoint : MonoBehaviour
 {
     private Animator anim;
     public bool activateStatus;
     public string id;
+    private Light2D light2D;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
+        light2D = GetComponentInChildren<Light2D>();
+    }
+
+    private void Update()
+    {
+        if(activateStatus == false)
+        {
+            light2D.enabled = false;
+        }
     }
 
     [ContextMenu("Generate Savepoint id")]
@@ -32,5 +43,6 @@ public class Savepoint : MonoBehaviour
             
         activateStatus = true;
         anim.SetBool("Active", true);
+        light2D.enabled = true;
     }
 }

2bdc644482c94526535b173235372e68a2d71efc
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2025-02-18 10:45:30 +0700

    Fix boss health bar UI

--- Assets/Scripts/EnemyScripts/DeathBringer/DeathBringerIdleState.cs
+++ Assets/Scripts/EnemyScripts/DeathBringer/DeathBringerIdleState.cs
@@ -19,7 +19,7 @@ public class DeathBringerIdleState : EnemyState
     public override void Update()
     {
         base.Update();
-        if(Vector2.Distance(player.transform.position, enemy.transform.position) < 7)
+        if(Vector2.Distance(player.transform.position, enemy.transform.position) < 7 && !enemy.bossFightBegun)
         {
             enemy.StartShowBossHealth();
         }

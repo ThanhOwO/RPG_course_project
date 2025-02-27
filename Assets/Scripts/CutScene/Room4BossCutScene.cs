251ceda32960764c5eb82fb444f9ccf9b15f489d
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2025-02-27 15:19:19 +0700

    Create skip cut scene function

--- Assets/Scripts/CutScene/Room4BossCutScene.cs
+++ Assets/Scripts/CutScene/Room4BossCutScene.cs
@@ -1,6 +1,8 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class Room4BossCutScene : MonoBehaviour
 {
@@ -16,6 +18,17 @@ public class Room4BossCutScene : MonoBehaviour
     public float barTargetSize = 100f;
     public float transitionSpeed = 2f;
 
+    [Header("Skip UI")]
+    public CanvasGroup skipUI;
+    public Image skipProgressBar;
+    public TextMeshProUGUI skipText;
+    private bool hasInteracted = false;
+
+    [Header("Skip Settings")]
+    public float holdTime = 3f;
+    private float holdTimer = 0f;
+    private bool canSkip = false;
+
     private void Start()
     {
         StartCoroutine(ShowCinematicBars());
@@ -25,6 +38,44 @@ public class Room4BossCutScene : MonoBehaviour
         PlayerManager.instance.player.isCutScene = true;
         timeline.Play();
         timeline.stopped += OnCutsceneEnd;
+        canSkip = true;
+        skipUI.alpha = 0;
+        skipProgressBar.fillAmount = 0f;
+    }
+
+    private void Update()
+    {
+        if (!hasInteracted && Input.anyKeyDown)
+        {
+            skipUI.alpha = 1;
+            hasInteracted = true;
+        }
+
+        if (canSkip && hasInteracted && Input.GetKey(KeyCode.E))
+        {
+            holdTimer += Time.deltaTime;
+            skipProgressBar.fillAmount = holdTimer / holdTime;
+
+            if (holdTimer >= holdTime)
+            {
+                SkipCutscene();
+            }
+        }
+        else
+        {
+            holdTimer = 0f;
+            skipProgressBar.fillAmount = 0f;
+        }
+    }
+
+    private void SkipCutscene()
+    {
+        if (!canSkip) return;
+
+        timeline.time = timeline.duration;
+        timeline.Evaluate();
+        OnCutsceneEnd(timeline);
+        canSkip = false;
     }
 
     private void OnCutsceneEnd(PlayableDirector obj)
@@ -38,6 +89,8 @@ public class Room4BossCutScene : MonoBehaviour
         boss.skipAppearState = true;
         boss.gameObject.SetActive(true);
         uiInGame.alpha = 1;
+        canSkip = false;
+        skipUI.alpha = 0;
     }
 
     private IEnumerator ShowCinematicBars()
